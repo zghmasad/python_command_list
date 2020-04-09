b1b21c93aefa3fcae9124b0bf987e98f522b69f6
ali sayfi <ali.35351@gmail.com>
2020-04-09 14:39:42 +0430

    second commitment

--- proccess.py
+++ proccess.py
@@ -18,3 +18,23 @@ if __name__=='__main__':
     p.start()
 
 print('Done')
+
+from multiprocessing import Pool
+from time import sleep
+
+
+def worker(x):
+    print('In worker with: ', x)
+    sleep(3)
+    return x * x
+
+
+if __name__ == '__main__':
+    with Pool(processes=4) as pool:
+        print(pool.map(worker, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+
+if __name__== '__main__':
+    with Pool(processes=4) as pool:
+        for result in pool.imap_unordered(worker,[1,2,3,4,5,6,7,8,9]):
+            print(result)
