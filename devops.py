0cdfc4f1a72d797c224f9271c05695bf5440732a
ali sayfi <ali.35351@gmail.com>
2020-04-24 21:13:03 +0430

    second commitment

--- devops.py
+++ devops.py
@@ -1,33 +1,8 @@
+import os
 
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
+print(os.stat('conf.yaml'))
 
-private_key = rsa.generate_private_key(public_exponent=65537,
-                                       key_size=4096,
-                                       backend=default_backend())
-
-#public_key = private_key.public_key
-#print(public_key)
-
-public_key = private_key.public_key()
-print(public_key)
-pk=private_key.public_key
-print(pk())
-print(private_key.public_key)
-
-message = b"More secrets go here"
-
-encrypted = public_key.encrypt(message,
-                               padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA512()),
-                                            algorithm=hashes.SHA512(),
-                                            label=None))
-
-print(encrypted)
-
-decrypted = private_key.decrypt(encrypted,
-                                padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA512()),
-                                             algorithm=hashes.SHA512(),
-                                             label=None))
-
-print(decrypted)
+cur=os.getcwd()
+print(os.path.split(cur))
+print(os.path.dirname(cur))
+print(os.path.basename(cur))
