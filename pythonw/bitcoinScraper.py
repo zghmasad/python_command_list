9182317d531358d346692e195a653a9faac80de0
ali35351 <ali.35351@gmail.com>
2020-05-10 13:15:11 +0430

    vs code

--- pythonw/bitcoinScraper.py
+++ pythonw/bitcoinScraper.py
@@ -1,14 +1,25 @@
 from time import sleep
+import urllib
 
 from bs4 import BeautifulSoup
 import requests
 
 while True:
     req = requests.get('https://www.tgju.org/')
-    #print(req.content)
     soup = BeautifulSoup(req.content, 'html.parser')
     findlist = soup.findAll(class_='info-price')
     print(findlist[5].get_text())
     print(findlist[8].get_text())
     sleep(10)
 
+'''#req = requests.get('https://www.tradingview.com/symbols/BTCUSD/technicals/')
+#print(req.content)
+url=r'https://www.tradingview.com/symbols/BTCUSD/technicals/'
+page = urllib.urlopen(url)
+html_content = page.read()
+rendered_content = html2text.html2text(html_content)
+with open(r'pythonw\text3.html','w+') as f:
+    f.write(str(rendered_content))
+#soup = BeautifulSoup(req.content, 'html.parser')
+#findlist = soup.findAll(class_='cell-5XzWwbDG')
+#print(findlist)'''
---
+++ pythonw/seleniumExample.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from time import sleep
+
+driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
+driver.get('http://www.google.com/')
+sleep(5) # Let the user actually see something!
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+with open('page.html', 'w') as f:
+    f.write(driver.page_source)
+
