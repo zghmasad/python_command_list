e5eb501a14d8c15a5d7f333714556c14c3f7a3a8
ali sayfi <ali.35351@gmail.com>
2020-05-21 09:05:33 +0430

    pycharm laptop

--- pythonw/bitcoin.py
+++ pythonw/bitcoin.py
@@ -5,26 +5,72 @@ from collections import deque
 from poloniex import Poloniex
 from time import sleep
 
+
+# declaration phase ---------------------------------------------------
+class Step:
+    def __init__(self):
+        self.count = 0
+        self.percent = 0
+        self.last_price = float(0)
+
+# time of period of observation
 period_of_observation = 10
 
+# poloniex object
 polo = Poloniex()
+
+# queues declaration
 queBTC = deque([])
 queTRX = deque([])
 queETH = deque([])
 queAVA = deque([])
 
+
+# initiation phase------------------------------------------------------
+dic = polo('returnTicker')
+first_step=Step()
+# BTC---------------------
+first_step.last_price=float(dic['USDT_BTC']['last'])
+queBTC.append(first_step)
+# AVA---------------------
+first_step.last_price=float(dic['USDT_AVA']['last'])
+queAVA.append(first_step)
+# TRX---------------------
+first_step.last_price=float(dic['USDT_TRX']['last'])
+queTRX.append(first_step)
+# ETH---------------------
+first_step.last_price=float(dic['USDT_ETH']['last'])
+queETH.append(first_step)
+
 for _ in range(period_of_observation):
     start_time = time.time()
     dic = polo('returnTicker')
-    queBTC.append(float(dic['USDT_BTC']['last']))
+    # BTC ----------------
+    newstep=Step()
+    newstep.last_price=float(dic['USDT_BTC']['last'])
+    laststep=queBTC[-1]
+    newstep.percent=(newstep.last_price-laststep.last_price)/laststep.last_price
+    if newstep.percent*laststep.percent>0:
+        pass
+    else:
+        pass
+    queBTC.append(newstep)
+    # AVA ----------------
     queAVA.append(float(dic['USDT_AVA']['last']))
+
+    # ETH ----------------
     queETH.append(float(dic['USDT_ETH']['last']))
+
+    # TRX ----------------
     queTRX.append(float(dic['USDT_TRX']['last']))
     x = time.time() - start_time
     if (x < 1):
         sleep(x)
 print(queBTC)
 
+
+
+# main phase -----------------------------------------------------------
 while True:
     start_time = time.time()
     dic = polo('returnTicker')
@@ -40,7 +86,6 @@ while True:
     queAVA.popleft()
     queETH.popleft()
     queTRX.popleft()
-
 
     x = time.time() - start_time
     if (x < 1):
